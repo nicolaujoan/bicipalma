e7ccf51f4434608ce3fb51f88c8c573e538e5bdf
nicolaujoan <jnicolausantandreu@cifpfb.eu>
2022-02-05 20:54:27 +0100

    ready

--- src/domain/java/estacion/Estacion.java
+++ src/domain/java/estacion/Estacion.java
@@ -1,12 +1,23 @@
 package estacion;
 
+import bicicleta.Movil;
+import tarjetausuario.Autenticacion;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Estacion {
     private final int id;
     private final String direccion;
+    private final int numeroAnclajes;
+    private final Anclajes anclajes;
 
-    public Estacion(int id, String direccion) {
+
+    public Estacion(int id, String direccion, int numeroAnclajes) {
         this.id = id;
         this.direccion = direccion;
+        this.numeroAnclajes = numeroAnclajes;
+        anclajes = new Anclajes(numeroAnclajes);
     }
 
     private int getId() {
@@ -19,6 +30,87 @@ public class Estacion {
 
     @Override
     public String toString() {
-        return "Estacion de " + direccion;
+        return "id: " + getId() +
+                "\ndireccion: " + getDireccion() +
+                "\nnumeroAnclajes: " + numAnclajes();
+
     }
+
+   private Anclaje[] anclajes() {
+        return anclajes.anclajes();
+    }
+
+    private int numAnclajes() {
+        return this.numeroAnclajes;
+    }
+
+    public void consultarEstacion() {
+        System.out.println(this);
+    }
+
+    public int anclajesLibres() {
+        return  (int) Arrays.stream(anclajes()).filter(a -> !a.isOcupado()).count();
+    }
+
+    // ancla la bici en cualquier anclaje libre
+    public void anclarBicicleta(Movil bici) {
+        Optional<Anclaje> anclajeLibre = Arrays.stream(anclajes())
+                                        .filter(anclaje -> !anclaje.isOcupado())
+                                        .findAny();
+        if (anclajeLibre.isPresent()) {
+            anclajeLibre.get().anclarBici(bici);
+        } else {
+            System.out.println("No existen enlaces disponibles para la bici " + bici);
+        }
+    }
+
+    // ancla la bici en el primer anclaje libre
+    public void anclarBicicleta(Movil bici, boolean primerAnclaje) {
+        Optional<Anclaje> anclajeLibre = Arrays.stream(anclajes())
+                                        .filter(anclaje -> !anclaje.isOcupado())
+                                        .findFirst();
+        if (anclajeLibre.isPresent()) {
+            anclajeLibre.get().anclarBici(bici);
+        } else {
+            System.out.println("No existen enlaces disponibles para la bici " + bici);
+        }
+    }
+
+    public boolean leerTarjetaUsuario(Autenticacion tarjeta) {
+        return tarjeta.isActivada();
+    }
+
+    public void retirarBicicleta(Autenticacion tarjeta) {
+        if (leerTarjetaUsuario(tarjeta)) {
+            Optional<Anclaje> anclajeOcupado = Arrays.stream(anclajes())
+                                               .filter(Anclaje::isOcupado)
+                                               .findAny();
+            if (anclajeOcupado.isPresent()) {
+                anclajeOcupado.get().liberarBici();
+            } else {
+                System.out.println("Todos los anclajes estan libres");
+            }
+
+        } else {
+            System.out.println("Tarjeta de usuario inactiva");
+        }
+    }
+
+    private void mostrarBicicleta(Movil bicicleta /*, int numeroAnclaje*/) {
+        System.out.println("bicicleta retirada: " + bicicleta.getId()
+                /*+ " del anclaje: " + numeroAnclaje*/);
+    }
+
+    private void mostrarAnclaje(Movil bicicleta, int numeroAnclaje) {
+        System.out.println("bicicleta " + bicicleta.getId()
+                + " anclada en el anclaje " + numeroAnclaje);
+    }
+
+    // info de los anclajes de la estación
+    public void consultarAnclajes() {
+        Arrays.stream(anclajes()).map(a -> Optional.ofNullable(a.getBici()))
+                        .forEach(bici -> System.out.println("Anclaje " +
+                        (bici.isPresent()? bici.get(): "libre\n")));
+    }
+
 }
--- test/java/estacion/EstacionTest.java
+++ test/java/estacion/EstacionTest.java
@@ -1,4 +1,28 @@
 package estacion;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class EstacionTest {
+
+//    Estacion estacion = null;
+
+   /* @Before
+    public void setup() {
+         estacion = new Estacion(5, "Petra", 20);
+    }*/
+
+    /*@Test
+    public void testConsultarEstacion() {
+        assertEquals("id: 5" +
+                "\ndireccion: Petra" +
+                "\nnumeroAnclajes: 20", estacion.toString());
+    }*/
+
+    @Test
+    public void visualizarAnclajesLibres() {
+        Estacion estacion = new Estacion(1, "Manacor", 6);
+        assertEquals(6, estacion.anclajesLibres());
+    }
 }
